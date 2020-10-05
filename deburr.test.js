2412d574489af587e187c1105a295f7b06de612f
Tran Dai Dung <trandaidung2810@gmail.com>
2020-10-05 10:21:17 +0700

    add more test

--- deburr.test.js
+++ deburr.test.js
@@ -1,9 +1,13 @@
 const deburr = require('./deburr')
 
-test("deburr 'déjà vu'", () => {
+test("deburr 'déjà vu' to 'deja vu'", () => {
   expect(deburr('déjà vu')).toEqual('deja vu')
 })
 
 test("deburr vietnamese's complete set", () => {
   expect(deburr('aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ')).toEqual('aAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAbBcCdDdDeEeEeEeEeEeEeEeEeEeEeEeEfFgGhHiIiIiIiIiIiIjJkKlLmMnNoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOpPqQrRsStTuUuUuUuUuUuUuUuUuUuUuUuUvVwWxXyYyYyYyYyYyYzZ')
+})
+
+test("deburr 'Hà Nội, Hồ Chí Minh' to 'Ha Noi, Ho Chi Minh'", () => {
+  expect(deburr('Hà Nội, Hồ Chí Minh')).toEqual('Ha Noi, Ho Chi Minh')
 })
